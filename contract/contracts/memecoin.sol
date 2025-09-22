0e569c55029e2208c39c674c88d4a6599408f868
Kingsterzgaming <prateekchauhan947@gmail.com>
2025-09-22 17:41:22 +0530

    buy function added

--- contract/contracts/memecoin.sol
+++ contract/contracts/memecoin.sol
@@ -10,9 +10,19 @@ contract Memecoin {
   uint8 _decimal = 18;
   uint256 public cap = 1000000;
   uint256 public mintedSupply ;
+  uint256 public saleCounter;
+  uint256 public saleEarning;
+  uint256 public preMintPrice;
+  uint256 public preMintSupply;
+  uint256 public preMintSupplyRemaining;
+  uint256 public minBuy;
+  uint256 public maxBuyLimit;
 
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner, string reason);
+  event OwnerMinted(address indexed owner , uint256 amount);
+  event mintDetailsSet(uint256 preMintPrice, uint256 minBuy, uint256 maxBuyLimit, uint256 preMintSupply);
   modifier onlyOwner() 
   {
     require(msg.sender == owner, "Caller is not the owner");
@@ -49,6 +59,25 @@ contract Memecoin {
         Balances[wallet] += circulationIncrease;
          emit Transfer(address(0),  wallet, circulationIncrease);
     }
+  }
+  
+  // mint to any address
+  function mint(uint256 amount, address to)  public onlyOwner()
+  {
+   require(mintedSupply + amount <= cap, "Exceeds market cap");
+   Balances[to] += amount;
+   mintedSupply += amount;
+   emit Transfer(address(0), to, amount);
+  }
+  
+  //mint owner only
+  function mintOwner(uint256 amount) public onlyOwner()
+  {
+    require(mintedSupply + amount <= cap, "Exceeds market cap");
+   Balances[msg.sender] += amount;
+   mintedSupply += amount;
+   emit Transfer(address(0), msg.sender, amount);
+   emit OwnerMinted(msg.sender, amount);
   }
 
   //approve tokens for spending
@@ -166,8 +195,50 @@ contract Memecoin {
 
 // crucial byt required function
 // ownersip transfer
-function transferOwnership(address newOwner) public onlyOwner {
+function transferOwnership(address newOwner , string  memory _reason) public onlyOwner 
+{
   require(newOwner != address(0), "New owner is the zero address");
-  owner = newOwner; 
+  owner = newOwner;
+  emit OwnershipTransferred(msg.sender, newOwner, _reason);
 }
+
+// token buy function for users
+function tokenBuy( address buyer, uint256 amount, uint256 cost ) public payable
+{
+  cost = amount * preMintPrice;
+  require(buyer != address(0), "Buyer is the zero address");
+  require(amount >= minBuy, "Buy amount is less than minimum buy");
+  require(amount<= maxBuyLimit , "Buy amount exceeds maximum buy limit");
+  require(msg.value >= cost, "Insufficient payment");
+  require(preMintSupplyRemaining >= amount, "Pre-mint supply exhausted");
+  require(Balances[buyer] + amount <= maxBuyLimit, "Purchase exceeds maximum buy limit");
+  require(preMintSupply >= preMintSupplyRemaining + amount, "exceeds premint supply");
+  Balances[buyer] += amount;
+  Balances[owner] -= amount;
+  preMintSupplyRemaining -= amount; 
+  saleEarning += msg.value;
+  saleCounter ++;
+  payable(owner).transfer(msg.value);
+  emit Transfer(owner, buyer, amount);
+}
+
+// set some values for token sale 
+function SetpreMintPrice(uint256 price, uint256 _minbuy, uint256 _maxLimit, uint256 _supply) public onlyOwner() 
+{
+  preMintPrice = price;
+  uint256 _conversion = 10 ** _decimal;
+  minBuy = _minbuy * _conversion; // 1 token
+  maxBuyLimit = _maxLimit * _conversion; // 1 token
+  require(Balances[owner] >= _supply * _conversion, "Owner balance is less than pre-mint supply");
+  preMintSupply = _supply * _conversion; // 1 token
+  preMintSupplyRemaining = preMintSupply;
+  saleCounter = 0;
+  emit mintDetailsSet(preMintPrice, minBuy, maxBuyLimit, preMintSupply);
+  // Chainlink integration and get live eth price is on hold  for now 
+}
+// set some values for token sale 
+// function DummySaleCounter(uint256 _saleCounter) public onlyOwner() 
+// {
+//   saleCounter = _saleCounter;
+// }
 }
